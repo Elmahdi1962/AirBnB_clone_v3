87e1a9ecd12414371a2cebab19cc20efb86c0efb
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-02-04 20:06:58 +0100

    update get test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -68,7 +68,7 @@ test_db_storage.py'])
                             "{:s} method needs a docstring".format(func[0]))
 
 
-class TestFileStorage(unittest.TestCase):
+class TestDBStorage(unittest.TestCase):
     """Test the FileStorage class"""
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_returns_dict(self):
@@ -90,15 +90,14 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_get(self):
         '''test that get method works properly'''
-        storage = DBStorage()
-        getobj = storage.get(State, '02314')
-        self.assertEqual(getobj, None)
-
-        newobj = City(name='zenaga')
-        newobj.save()
-        obj_id = newobj.id
-        getobj = storage.get(City, obj_id)
-        self.assertEqual(getobj, newobj)
+        new_state = State(name="New York")
+        new_state.save()
+        new_user = User(email="bob@foobar.com", password="password")
+        new_user.save()
+        self.assertIs(new_state, models.storage.get("State", new_state.id))
+        self.assertIs(None, models.storage.get("State", "blah"))
+        self.assertIs(None, models.storage.get("blah", "blah"))
+        self.assertIs(new_user, models.storage.get("User", new_user.id))
 
     @unittest.skipIf(models.storage_t != 'db', "not testing file storage")
     def test_count(self):
